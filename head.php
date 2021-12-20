9c3d03d1a39d7d50a36146029cedb7316408824e
Greeny385 <krugeradrean@gmail.com>
2021-12-20 11:55:41 +0100

    Grundstruktur des Heads hinzugefügt

---
+++ head.php
@@ -0,0 +1,27 @@
+<meta charset="UTF-8">
+<meta name="description" content="Homepage Friedrich-Gymnasium Luckenwalde">
+<meta name="author" content="Adrean K. & Jannick H. & Florian P.">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="version" content="closed Beta">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="stylesheet" href="/css/main_dark.css">
+<!--<link rel="stylesheet" href="/css/headfoot.css">
+<link rel="stylesheet" href="/css/headfoot_dark.css">-->
+<link rel="icon" href="/img/logo.png">
+
+<!--
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+-->
+        
+        
+<!-- OpenGraph Protocol -->
+<meta property="og:title" content="Friedrich-Gymnasium Luckenwalde">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://www.friedrich-gymnasium.de/">
+<meta property="og:description" content="Dies ist die offizielle Homepage des Friedrich-Gymnasiums aus Luckenwalde">
+<meta property="og:image" content="https://www.friedrich-gymnasium.de/img/logo.png">
+
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="de-DE" prefix="og: https://ogp.me/ns#" xmlns:og="http://opengraphprotocol.org/schema/">
+    <head>
+        <?php
+            include_once "head.php"
+        ?>
+        <title>Startseite - Friedrich-Gymnasium Luckenwalde</title>
+    </head>
+    <body>
+
+<?php
+
+    echo "Hello World"
+
+?>
+
+    </body>
+</html>
