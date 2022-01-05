3d57dbed01b302f8dc5704de2e1638250b315627
Agent-Husky <agent-husky@gmx.de>
2022-01-05 17:10:27 +0100

    link to school email portal

---
+++ schul-email/index.php
@@ -0,0 +1,18 @@
+<?php header("Location: https://webmail.all-inkl.com/"); ?>
+<html>
+    <head>
+        <?php 
+
+            include_once "./../sites/head.html"
+
+        ?>
+        <title>Email - Friedrich-Gymnasium Luckenwalde</title>
+    </head>
+    <body>
+        <?php 
+
+            include_once "./../sites/header.html"
+
+        ?>
+    </body>
+</html>
