7022e7d23166b7cb509b42e3535cc1efd4310624
Agent-Husky <agent-husky@gmx.de>
2022-01-05 17:10:41 +0100

    link to fb page of the 'förderverein'

---
+++ foerderverein-fb/index.php
@@ -0,0 +1,18 @@
+<?php header("Location: https://www.facebook.com/foerderverein.fgl/"); ?>
+<html>
+    <head>
+        <?php 
+
+            include_once "./../sites/head.html"
+
+        ?>
+        <title>Förderverein - Friedrich-Gymnasium Luckenwalde</title>
+    </head>
+    <body>
+        <?php 
+
+            include_once "./../sites/header.html"
+
+        ?>
+    </body>
+</html>
