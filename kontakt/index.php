408affb3b81b7a01a3e551c5c7d862eef4fdf3c8
Agent-Husky <agent-husky@gmx.de>
2022-01-12 21:16:11 +0100

    v1 of the contact page

---
+++ kontakt/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="de-DE" prefix="og: https://ogp.me/ns#" xmlns:og="http://opengraphprotocol.org/schema/">
+    <head>
+        <?php
+
+            include_once "./../sites/head.html"
+
+        ?>
+        <title>News - Friedrich-Gymnasium Luckenwalde</title>
+    </head>
+    <body>
+        <?php
+
+            include_once "./../sites/header.html"
+
+        ?>
+        <div class="page-beginning"></div>
+        <div class="contact-page">
+            <h1>Kontakt</h1>
+            <div class="adresse">
+                <div class="mapdiv"><iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9806.967546925716!2d13.175272!3d52.0844284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x619e482eecee030f!2sFriedrich-Gymnasium!5e0!3m2!1sde!2sde!4v1642010959150!5m2!1sde!2sde" loading="lazy"></iframe></div>
+                <div class="adresse-text">
+                    <i class="fas fa-map-marker-alt"> Adresse</i>
+                    <p>Parkstraße 59<br>14943 Luckenwalde</p>
+                </div>
+            </div>
+            <div class="clear-float"></div>
+            <div class="contact">
+                <a href="tel:+49-176-6666-4887"><div><i class="fas fa-phone-square-alt"> <p>Telefonnummer</p></i><p>+49 3371 632569</p></div></a>
+                <div><i class="fas fa-fax"> <p>Fax</p></i><p>+49 3371 641060</p></div>
+                <a href="mailto:s120534@schulen.brandenburg.de"><div><i class="fas fa-envelope"> <p>Email</p></i><p>s120534@schulen.brandenburg.de</p></div></a>
+                <div><i class="far fa-clock"> <p>Öffnungszeiten des Sekretariat</p></i><p>Mo-Do 7:00-12:15 12:45-15:00; Fr 7:00-13:00</p></div>
+            </div>
+        </div>
+        <br>
+        <div class="page-ending"></div>
+    </body>
+</html>
