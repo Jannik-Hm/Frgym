85758e1b6843d3b5d5d13b69d7ea29d75c1a50bb
Greeny385 <krugeradrean@gmail.com>
2022-01-12 21:19:26 +0100

    startseite started

--- index.php
+++ index.php
@@ -8,14 +8,23 @@
         ?>
         <title>Startseite - Friedrich-Gymnasium Luckenwalde</title>
     </head>
-    <body>
+    <body style="height: 200%">
         <?php 
 
             include_once "./sites/header.html" 
 
         ?>
 
-
-        
+        <div class="parallax"></div>
+        <section id="mainTop">
+            <div>
+                <h1>Friedrich-Gymnasium</h1>
+                <h2>Luckenwalde</h2>
+                <i class="far fa-arrow-alt-circle-down" style="font-size: 60px; color: #ffffffa6;" id="upndown"></i>
+            </div>
+        </section>
+        <section style="top: calc(100% - 50px);height: 30%; width: 100%; background-color: black">
+            test
+        </section>
     </body>
 </html>
