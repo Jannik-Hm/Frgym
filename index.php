e740f6b615adc6de1bbf6091367320596609800b
Greeny385 <krugeradrean@gmail.com>
2022-01-14 20:45:03 +0100

    startseite changes

--- index.php
+++ index.php
@@ -1,33 +1,47 @@
 <!DOCTYPE html>
 <html lang="de-DE" prefix="og: https://ogp.me/ns#" xmlns:og="http://opengraphprotocol.org/schema/">
-    <head>
-        <?php 
 
-            include_once "./sites/head.html" 
+<head>
+    <?php
 
-        ?>
-        <title>Startseite - Friedrich-Gymnasium Luckenwalde</title>
-    </head>
-    <body style="height: 200%">
-        <?php 
+    include_once "./sites/head.html"
 
-            include_once "./sites/header.html" 
+    ?>
+    <title>Startseite - Friedrich-Gymnasium Luckenwalde</title>
+</head>
 
-        ?>
+<body style="height: 200%">
+    <?php
 
-        <div class="parallax"></div>
-        <section id="mainTop">
+    include_once "./sites/header.html"
+
+    ?>
+
+    <div class="parallax"></div>
+    <section id="mainTop">
+        <div>
+            <h1>Friedrich-Gymnasium</h1>
+            <h2>Luckenwalde</h2>
+            <i class="far fa-arrow-alt-circle-down" style="font-size: 60px; color: #ffffffa6;" id="upndown"></i>
+        </div>
+    </section>
+    <section id="mainSecond">
+        <div>
             <div>
-                <h1>Friedrich-Gymnasium</h1>
-                <h2>Luckenwalde</h2>
-                <i class="far fa-arrow-alt-circle-down" style="font-size: 60px; color: #ffffffa6;" id="upndown"></i>
+                <img src="https://via.placeholder.com/200?text=Placeholder">
             </div>
-        </section>
-        <section style="top: calc(100% - 50px);height: 30%; width: 100%; background-color: black">
-            <div style="padding-top: 30px;">
-                <p style="border: 1px solid #3d859c; border-radius: 20px; width: fit-content; margin-left: auto; margin-right: auto; padding: 5px 20px; font-size: 25px; ">Adresse</p>
-                <p>Parkstraße 59<br>14943 Luckenwalde<br><br>Telefon:  03371-632569<br>Fax:       03371-641060<br>E-Mail:   s120534@schulen.brandenburg.de</p>
+            <div>
+                <p>Adresse</p>
+                <p>Parkstraße 59<br>14943 Luckenwalde<br><br>Telefon: 03371-632569<br>Fax: 03371-641060<br>E-Mail: s120534@schulen.brandenburg.de</p>
             </div>
-        </section>
-    </body>
+            <div>
+                <img src="https://via.placeholder.com/200?text=Placeholder">
+            </div>
+        </div>
+    </section>
+    <section style="top: calc(100% - 50px); height: fit-content; width: 100%; background-color: black">
+
+    </section>
+</body>
+
 </html>
