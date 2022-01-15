dcfdd568b1ef5d252e0721c9407843016af66092
Agent-Husky <agent-husky@gmx.de>
2022-01-15 19:58:48 +0100

    cleanup of the contact page

--- kontakt/index.php
+++ kontakt/index.php
@@ -20,16 +20,30 @@
             <div class="adresse">
                 <div class="mapdiv"><iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9806.967546925716!2d13.175272!3d52.0844284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x619e482eecee030f!2sFriedrich-Gymnasium!5e0!3m2!1sde!2sde!4v1642010959150!5m2!1sde!2sde" loading="lazy"></iframe></div>
                 <div class="adresse-text">
-                    <i class="fas fa-map-marker-alt"> Adresse</i>
+                    <i class="fas fa-map-marker-alt"><p>Adresse</p></i>
                     <p>Parkstraße 59<br>14943 Luckenwalde</p>
                 </div>
             </div>
             <div class="clear-float"></div>
             <div class="contact">
-                <a href="tel:+49-176-6666-4887"><div><i class="fas fa-phone-square-alt"> <p>Telefonnummer</p></i><p>+49 3371 632569</p></div></a>
+                <a href="tel:+49-3371-632569"><div><i class="fas fa-phone-square-alt"> <p>Telefonnummer</p></i><p>+49 3371 632569</p></div></a>
                 <div><i class="fas fa-fax"> <p>Fax</p></i><p>+49 3371 641060</p></div>
                 <a href="mailto:s120534@schulen.brandenburg.de"><div><i class="fas fa-envelope"> <p>Email</p></i><p>s120534@schulen.brandenburg.de</p></div></a>
-                <div><i class="far fa-clock"> <p>Öffnungszeiten des Sekretariat</p></i><p>Mo-Do 7:00-12:15 12:45-15:00; Fr 7:00-13:00</p></div>
+                <br>
+                <div>
+                    <i class="far fa-clock"> <p>Öffnungszeiten des Sekretariat</p></i>
+                    <br>
+                    <div class="left">
+                        <h3>Mo - Do</h3>
+                        <p>7:00-12:15</p>
+                        <p>12:45-15:00</p>
+                    </div>
+                    <div class="right">
+                        <h3>Fr</h3>
+                        <p>7:00-13:00</p>
+                        <br>
+                    </div>
+                </div>
             </div>
         </div>
         <br>
