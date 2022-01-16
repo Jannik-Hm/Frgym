4e2b7fcef7498e2d7a0c710c14aa6b1638468ab6
Greeny385 <krugeradrean@gmail.com>
2022-01-16 22:48:24 +0100

    startseite changes, footer block

--- index.php
+++ index.php
@@ -10,20 +10,19 @@
     <title>Startseite - Friedrich-Gymnasium Luckenwalde</title>
 </head>
 
-<body style="height: 200%">
+<body>
     <?php
 
     include_once "./sites/header.html"
 
     ?>
 
-    <div class="parallax"></div>
-    <section id="mainTop">
+    <section id="mainTop" class="parallax">
         <div>
             <h1>Friedrich-Gymnasium</h1>
             <h2>Luckenwalde</h2>
-            <i class="far fa-arrow-alt-circle-down" style="font-size: 60px; color: #ffffffa6;" id="upndown"></i>
         </div>
+        <i class="far fa-arrow-alt-circle-down" style="font-size: 250%; color: #ffffffa6; position:sticky; top: 85%; background-color: rgb(60, 60, 60, 0.6); padding: 5px; border-radius: 50px; margin-bottom: 100px" id="upndown"></i>
     </section>
     <section id="mainSecond">
         <div>
@@ -32,16 +31,32 @@
             </div>
             <div>
                 <p>Adresse</p>
-                <p>Parkstraße 59<br>14943 Luckenwalde<br><br>Telefon: 03371-632569<br>Fax: 03371-641060<br>E-Mail: s120534@schulen.brandenburg.de</p>
+                <p>Parkstraße 59<br>14943 Luckenwalde<br><br>Telefon: <a href="tel:+493371632569">03371-632569</a><br>Fax: <a href="tel:+493371641060">03371-641060</a><br><a href="mailto:s120534@schulen.brandenburg.de">E-Mail</a> </p>
             </div>
             <div>
                 <img src="https://via.placeholder.com/200?text=Placeholder">
             </div>
         </div>
+        <span class="line"></span>
     </section>
-    <section style="top: calc(100% - 50px); height: fit-content; width: 100%; background-color: black">
+    <section id="mainThird">
+        <div>
+            <p>Über uns</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nulla rem iste quaerat iure tenetur repudiandae minima, aliquid in optio eaque soluta voluptatum corrupti ullam. Id, fugit! Assumenda, vel quas.</p>
+        </div>
+    </section>
+    <!-- <section style="height: fit-content; width: 100%; background-color: #1a1a1a; display: block; position: relative; margin: 0; padding: 0;">
+        <div>
+            <p>Aktuelles</p>
+            NEWS Embed
+        </div>
+    </section> -->
 
-    </section>
+    <?php
+
+    include_once "./sites/footer.html"
+
+    ?>
 </body>
 
 </html>
