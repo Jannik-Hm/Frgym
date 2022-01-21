d7f0ee3c77293d7d56fadae77aa9712506759c34
Agent-Husky <agent-husky@gmx.de>
2022-01-21 20:52:58 +0100

    v1 of news-slider

---
+++ news/news-slide.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="de-DE" prefix="og: https://ogp.me/ns#" xmlns:og="http://opengraphprotocol.org/schema/">
+
+<head>
+    <?php
+
+    include_once "./../sites/head.html"
+
+    ?>
+    <title>News-Slide - Friedrich-Gymnasium Luckenwalde</title>
+</head>
+
+<body>
+    <div class="newsslidershow">
+    <?php
+                $lesscharnum = 600;
+                $lessrownumber = 6;
+                $rowcharnum = 600;
+
+                require "./../sites/credentials.php";
+                $conn = get_connection();
+
+                $sql = "SELECT * FROM news ORDER BY zeit DESC LIMIT 3";
+                $result = mysqli_query($conn,$sql);
+                if ($result->num_rows > 0) {
+                    $i = 0;
+                    while($row = $result->fetch_assoc()) {
+                        $news[$i]["id"] = $row["id"];
+                        $news[$i]["titel"] = $row["titel"];
+                        $news[$i]["inhalt"] = $row["inhalt"];
+                        $news[$i]["autor"] = $row["autor"];
+                        $news[$i]["zeit"] = $row["zeit"];
+                        $i++;
+                    }
+                }
+            ?>
+            <div class="slider">
+                <div class="newsslides">
+                    <input type="radio" name="radio-btn" id="radio1">
+                    <input type="radio" name="radio-btn" id="radio2">
+                    <input type="radio" name="radio-btn" id="radio3">
+                    <br>
+                    <?php
+                        for ($i=0; $i<count($news); $i++){
+                            $id = $news[$i]["id"];
+                            echo("<div class='newsslide". ($i+1) ."'>");
+                                echo("<a class='divrm".$i."'><div class='news'>");
+                                    $title = $news[$i]["titel"];
+                                    $inhalt = $news[$i]["inhalt"];
+                                    $lessinhalt = substr($news[$i]["inhalt"],0,$lesscharnum);
+                                    if (strlen($inhalt) > strlen($lessinhalt)) {
+                                        $lessinhalt = $lessinhalt . "...";
+                                    }
+                                    $autor = $news[$i]["autor"];
+                                    $zeitor1 = explode("-", $news[$i]["zeit"]);
+                                    $zeitor2 = explode(" ", $zeitor1[2]);
+                                    $zeitor3 = explode(":", $zeitor2[1]);
+                                    $zeit = $zeitor2[0] . "." . $zeitor1[1] . "." . $zeitor1[0] . " " . $zeitor3[0] . ":" . $zeitor3[1] . " Uhr";
+                                    echo("<h1>".$title."<br>
+                                        <h5><p>Veröffentlicht von ".$autor."</p><p class='time'>am ".$zeit."</p></h5>"."</h1>");
+                                    echo("<p>".nl2br($lessinhalt)." <a class='readmore".$i."'>Mehr anzeigen</a></p>");
+                                echo("</div></a>");
+                                echo("<div style='left: 0;' class='readmorebox".$i."'>
+                                    <span class='helper'></span>
+                                    <div class='scroll'>
+                                        <div class='popupCloseButton".$i."'>&times;</div>
+                                        <div class='newspopup'>
+                                            <h1>".$title."<br>
+                                                <h5><p>Veröffentlicht von ".$autor."</p><p class='time'>am ".$zeit."</p></h5>
+                                            </h1>
+                                            <p>".nl2br($inhalt)."</p>
+                                        </div>
+                                    </div>
+                                </div>");
+                                echo("<script>$(window).load(function () {
+                                    $('.readmore".$i."').click(function(){
+                                    $('.readmorebox".$i."').show();
+                                    });
+                                    $('.divrm".$i."').click(function(){
+                                        $('.readmorebox".$i."').show();
+                                        });
+                                    $('.popupCloseButton".$i."').click(function(){
+                                        $('.readmorebox".$i."').hide();
+                                    });
+                                });</script>");
+                            echo("</div>");
+                        }
+                    ?>
+                    <div class="navigation-auto">
+                        <div class="auto-btn1"></div>
+                        <div class="auto-btn2"></div>
+                        <div class="auto-btn3"></div>
+                    </div>
+                </div>
+                <div class="navigation-manual">
+                    <label for="radio1" class="manual-btn"></label>
+                    <label for="radio2" class="manual-btn"></label>
+                    <label for="radio3" class="manual-btn"></label>
+                </div>
+            </div>
+            <script type="text/javascript">
+                var counter = 1;
+                setInterval(function(){
+                document.getElementById('radio' + counter).checked = true;
+                counter++;
+                if(counter > 3){
+                    counter = 1;
+                }
+                }, 5000);
+            </script>
+        </div>
+    </body>
+</html>
