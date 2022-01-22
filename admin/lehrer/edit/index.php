96adf4957ba3057a303100c9035b4640bf62807a
Agent-Husky <agent-husky@gmx.de>
2022-01-22 18:14:10 +0100

    added permissions and absolute paths to edit teacher form

--- admin/lehrer/edit/index.php
+++ admin/lehrer/edit/index.php
@@ -13,7 +13,9 @@
     <head>
         <?php
 
-            include_once "./../../sites/head.html"
+            $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+
+            include_once "$root/admin/sites/head.html";
 
         ?>
         <title>Lehrer bearbeiten - Admin Panel - Friedrich-Gymnasium Luckenwalde</title>
@@ -21,8 +23,11 @@
     <body>
         <?php
 
-            include_once "./../../sites/header.html"
+            include_once "$root/admin/sites/header.html";
 
+            include_once "$root/admin/sites/permissions.php";
+
+            include_once "$root/admin/no-permission.html";
         ?>
 
         <?php
@@ -52,21 +57,29 @@
             $infotext = $row["beschreibung"];
         }
 
+        if($lehrer_own == 1 && $_SESSION["vorname"] == $vorname && $_SESSION["nachname"] == $nachname){
+            $ownedit = true;
+            $disabled = false;
+        }elseif($lehrer_all == 0){
+            echo("<script>$('.no_perm').show();</script>");
+            $disabled = true;
+        };
+
         ?>
 
         <div class="add-input">
             <form method="POST">
-                <input type="text" width="" placeholder="Vorname*" name="vorname" value="<?php echo $vorname; ?>" required><br>
-                <input type="text" placeholder="Nachname*" name="nachname" value="<?php echo $nachname; ?>" required><br>
-                <input type="email" placeholder="Email*" name="email" value="<?php echo $email; ?>" required><br>
+                <input type="text" width="" placeholder="Vorname*" name="vorname" value="<?php echo $vorname; ?>"  <?php if($disabled or $ownedit){echo("disabled");} ?> required><br>
+                <input type="text" placeholder="Nachname*" name="nachname" value="<?php echo $nachname; ?>" <?php if($disabled or $ownedit){echo("disabled");} ?> required><br>
+                <input type="email" placeholder="Email*" name="email" value="<?php echo $email; ?>" <?php if($disabled or $ownedit){echo("disabled");} ?> required><br>
                 <div class="position">
                     <label class="heading2">Position</label>
                     <ul>
-                        <li><label><input type="radio" name="position" value="Lehrer*in" <?php if ($position == 'Lehrer*in') echo "checked"; ?>>Lehrer*in</label></li>
-                        <li><label><input type="radio" name="position" value="Schulleiter*in" <?php if ($position == 'Schulleiter*in') echo "checked"; ?>>Schulleiter*in</label></li>
-                        <li><label><input type="radio" name="position" value="stellvertretender Schulleiter*in" <?php if ($position == 'stellvertretender Schulleiter*in') echo "checked"; ?>>stellvertretender Schulleiter*in</label></li>
-                        <li><label><input type="radio" name="position" value="Oberstufenkoordinator*in" <?php if ($position == 'Oberstufenkoordinator*in') echo "checked"; ?>>Oberstufenkooridnator*in</label></li>
-                        <li><label><input type="radio" name="position" value="Sekretär*in" <?php if ($position == 'Sekretär*in') echo "checked"; ?>>Sekretär*in</label></li>
+                        <li><label><input type="radio" name="position" value="Lehrer*in" <?php if ($position == 'Lehrer*in') echo "checked"; ?> <?php if($disabled or $ownedit){echo("disabled");} ?>>Lehrer*in</label></li>
+                        <li><label><input type="radio" name="position" value="Schulleiter*in" <?php if ($position == 'Schulleiter*in') echo "checked"; ?> <?php if($disabled or $ownedit){echo("disabled");} ?>>Schulleiter*in</label></li>
+                        <li><label><input type="radio" name="position" value="stellvertretender Schulleiter*in" <?php if ($position == 'stellvertretender Schulleiter*in') echo "checked"; ?> <?php if($disabled or $ownedit){echo("disabled");} ?>>stellvertretender Schulleiter*in</label></li>
+                        <li><label><input type="radio" name="position" value="Oberstufenkoordinator*in" <?php if ($position == 'Oberstufenkoordinator*in') echo "checked"; ?> <?php if($disabled or $ownedit){echo("disabled");} ?>>Oberstufenkooridnator*in</label></li>
+                        <li><label><input type="radio" name="position" value="Sekretär*in" <?php if ($position == 'Sekretär*in') echo "checked"; ?> <?php if($disabled or $ownedit){echo("disabled");} ?>>Sekretär*in</label></li>
                         <br>
                     </ul>
                     <br>
@@ -76,51 +89,51 @@
                     <ul>
                         <ul>
                             <label class="heading">Sprachwissenschaften</label>
-                            <li><label><input type="checkbox" name="chk_group[]" value="DE" <?php foreach ($faecher as $fach) if ($fach == "DE") echo "checked" ; ?>>Deutsch</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="EN" <?php foreach ($faecher as $fach) if ($fach == "EN") echo "checked" ; ?>>Englisch</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="FR" <?php foreach ($faecher as $fach) if ($fach == "FR") echo "checked" ; ?>>Französisch</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="PO" <?php foreach ($faecher as $fach) if ($fach == "PO") echo "checked" ; ?>>Polnisch</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="RU" <?php foreach ($faecher as $fach) if ($fach == "RU") echo "checked" ; ?>>Russisch</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="SN" <?php foreach ($faecher as $fach) if ($fach == "SN") echo "checked" ; ?>>Spanisch</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="TR" <?php foreach ($faecher as $fach) if ($fach == "TR") echo "checked" ; ?>>Türkisch</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="LA" <?php foreach ($faecher as $fach) if ($fach == "LA") echo "checked" ; ?>>Latein</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="DE" <?php foreach ($faecher as $fach) if ($fach == "DE") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Deutsch</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="EN" <?php foreach ($faecher as $fach) if ($fach == "EN") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Englisch</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="FR" <?php foreach ($faecher as $fach) if ($fach == "FR") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Französisch</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="PO" <?php foreach ($faecher as $fach) if ($fach == "PO") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Polnisch</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="RU" <?php foreach ($faecher as $fach) if ($fach == "RU") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Russisch</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="SN" <?php foreach ($faecher as $fach) if ($fach == "SN") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Spanisch</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="TR" <?php foreach ($faecher as $fach) if ($fach == "TR") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Türkisch</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="LA" <?php foreach ($faecher as $fach) if ($fach == "LA") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Latein</label></li>
                         </ul>
                         <ul>
                             <label class="heading">Naturwissenschaften</label>
-                            <li><label><input type="checkbox" name="chk_group[]" value="MA" <?php foreach ($faecher as $fach) if ($fach == "MA") echo "checked" ; ?>>Mathe</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="BI" <?php foreach ($faecher as $fach) if ($fach == "BI") echo "checked" ; ?>>Biologie</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="CH" <?php foreach ($faecher as $fach) if ($fach == "CH") echo "checked" ; ?>>Chemie</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="PH" <?php foreach ($faecher as $fach) if ($fach == "PH") echo "checked" ; ?>>Physik</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="IF" <?php foreach ($faecher as $fach) if ($fach == "IF") echo "checked" ; ?>>Informatik</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="NW" <?php foreach ($faecher as $fach) if ($fach == "NW") echo "checked" ; ?>>Naturwissenschaften</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="MA" <?php foreach ($faecher as $fach) if ($fach == "MA") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Mathe</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="BI" <?php foreach ($faecher as $fach) if ($fach == "BI") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Biologie</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="CH" <?php foreach ($faecher as $fach) if ($fach == "CH") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Chemie</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="PH" <?php foreach ($faecher as $fach) if ($fach == "PH") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Physik</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="IF" <?php foreach ($faecher as $fach) if ($fach == "IF") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Informatik</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="NW" <?php foreach ($faecher as $fach) if ($fach == "NW") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Naturwissenschaften</label></li>
                         </ul>
                         <ul>
                             <label class="heading">Gesellschaftswissenschaften</label>
-                            <li><label><input type="checkbox" name="chk_group[]" value="EK" <?php foreach ($faecher as $fach) if ($fach == "EK") echo "checked" ; ?>>Erdkunde</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="GE" <?php foreach ($faecher as $fach) if ($fach == "GE") echo "checked" ; ?>>Geschichte</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="PB" <?php foreach ($faecher as $fach) if ($fach == "PB") echo "checked" ; ?>>Politische Bildung</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="EG" <?php foreach ($faecher as $fach) if ($fach == "EG") echo "checked" ; ?>>Gesellschaftswissenschaften</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="RE" <?php foreach ($faecher as $fach) if ($fach == "RE") echo "checked" ; ?>>Evangelischer Religionsunterricht</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="RK" <?php foreach ($faecher as $fach) if ($fach == "RK") echo "checked" ; ?>>Katholischer Religionsunterricht</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="LE" <?php foreach ($faecher as $fach) if ($fach == "LE") echo "checked" ; ?>>Lebensgestaltung-Ethik-Religionskunde</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="AL" <?php foreach ($faecher as $fach) if ($fach == "AL") echo "checked" ; ?>>Wirtschaft-Arbeit-Technik</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="WW" <?php foreach ($faecher as $fach) if ($fach == "WW") echo "checked" ; ?>>Wirtschaftswissenschaften</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="EK" <?php foreach ($faecher as $fach) if ($fach == "EK") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Erdkunde</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="GE" <?php foreach ($faecher as $fach) if ($fach == "GE") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Geschichte</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="PB" <?php foreach ($faecher as $fach) if ($fach == "PB") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Politische Bildung</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="EG" <?php foreach ($faecher as $fach) if ($fach == "EG") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Gesellschaftswissenschaften</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="RE" <?php foreach ($faecher as $fach) if ($fach == "RE") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Evangelischer Religionsunterricht</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="RK" <?php foreach ($faecher as $fach) if ($fach == "RK") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Katholischer Religionsunterricht</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="LE" <?php foreach ($faecher as $fach) if ($fach == "LE") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Lebensgestaltung-Ethik-Religionskunde</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="AL" <?php foreach ($faecher as $fach) if ($fach == "AL") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Wirtschaft-Arbeit-Technik</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="WW" <?php foreach ($faecher as $fach) if ($fach == "WW") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Wirtschaftswissenschaften</label></li>
                         </ul>
                         <ul>
                             <label class="heading">Künstlerische Fächer</label>
-                            <li><label><input type="checkbox" name="chk_group[]" value="DS" <?php foreach ($faecher as $fach) if ($fach == "DS") echo "checked" ; ?>>Darstellendes Spiel</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="KU" <?php foreach ($faecher as $fach) if ($fach == "KU") echo "checked" ; ?>>Kunst</label></li>
-                            <li><label><input type="checkbox" name="chk_group[]" value="MU" <?php foreach ($faecher as $fach) if ($fach == "MU") echo "checked" ; ?>>Musik</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="DS" <?php foreach ($faecher as $fach) if ($fach == "DS") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Darstellendes Spiel</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="KU" <?php foreach ($faecher as $fach) if ($fach == "KU") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Kunst</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="MU" <?php foreach ($faecher as $fach) if ($fach == "MU") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Musik</label></li>
                         </ul>
                         <ul>
                             <label class="heading">Sonstige</label>
-                            <li><label><input type="checkbox" name="chk_group[]" value="SP" <?php foreach ($faecher as $fach) if ($fach == "SP") echo "checked" ; ?>>Sport</label></li>
+                            <li><label><input type="checkbox" name="chk_group[]" value="SP" <?php foreach ($faecher as $fach) if ($fach == "SP") echo "checked" ; ?> <?php if($disabled){echo "disabled";} ?>>Sport</label></li>
                         </ul>
                     </ul>
                 </div>
-                <input type="text" placeholder="Infotext (Optional)" name="beschreibung" value="<?php echo $infotext; ?>"><br>
-                <input type="date" placeholder="Geburtstag (Optional)" name="geburtstag" value="<?php echo $date; ?>" Optional><br>
-                <input type="submit" name="submit" value="Speichern">
+                <input type="text" placeholder="Infotext (Optional)" name="beschreibung" value="<?php echo $infotext; ?>" <?php if($disabled){echo "disabled";} ?>><br>
+                <input type="date" placeholder="Geburtstag (Optional)" name="geburtstag" value="<?php echo $date; ?>" Optional <?php if($disabled){echo "disabled";} ?>><br>
+                <input type="submit" name="submit" <?php if($disabled){echo "disabled";} ?> value="Speichern">
                 <div class="page-ending"></div>
             </form>
         </div>
@@ -162,11 +175,13 @@
                     $faecher = $faecher.";";
                 }
             }
-            if(isset($_POST["submit"])) {
+            if(isset($_POST["submit"]) && ($disabled == false && $ownedit == false)) {
                 $insert = mysqli_query($conn, "UPDATE lehrer SET vorname='{$vorname}', nachname='{$nachname}', email='{$email}', position=NULLIF('{$position}', ''), faecher='{$faecher}', beschreibung=NULLIF('{$infotext}', ''), datum=NULLIF('{$geburtstag}','') WHERE id='{$id}'");
-                if ($insert) {
-                    echo("<script>$('.confirm').show();</script>");
-                }
+            }elseif(isset($_POST["submit"]) && ($disabled == false && $ownedit)){
+                $insert = mysqli_query($conn, "UPDATE lehrer SET faecher='{$faecher}', beschreibung=NULLIF('{$infotext}', ''), datum=NULLIF('{$geburtstag}','') WHERE id='{$id}'");
+            }
+            if ($insert) {
+                echo("<script>$('.confirm').show();</script>");
             }
         ?>
     </body>
