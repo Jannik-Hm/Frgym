e3fcf7e8e3ae6f45de9505f6b6d5fe5f81414e0f
Agent-Husky <agent-husky@gmx.de>
2022-01-23 11:41:07 +0100

    same for lehrer admin table

--- admin/lehrer/index.php
+++ admin/lehrer/index.php
@@ -66,16 +66,8 @@
 
         <?php
 
-            $servername = "sql150.your-server.de";
-            $username = "c0921922321";
-            $password = "AHWNiBfs2u14AAZg"; //master
-            $dbname = "friedrich_gym";
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
+            require_once "$root/sites/credentials.php";
+            $conn = get_connection();
 
             if(!isset($_GET["id"])) {
             //output every lehrer 
