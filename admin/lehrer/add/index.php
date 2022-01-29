3cf1f1b3436b54953b8cc098c3d2d57f18db4777
Agent-Husky <agent-husky@gmx.de>
2022-01-29 22:08:33 +0100

    removed all file upload error warnings for now

--- admin/lehrer/add/index.php
+++ admin/lehrer/add/index.php
@@ -213,24 +213,24 @@
 
                         // Check file size
                         if ($_FILES["pictureUpload"]["size"] > 10000000) {
-                            echo "Sorry, your file is too large.";
+                            // echo "Sorry, your file is too large.";
                             $uploadOk = 0;
                         }
 
                         // Allow certain file formats
                         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-                            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                            // echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
                             $uploadOk = 0;
                         }
                         // Check if $uploadOk is set to 0 by an error
                         if ($uploadOk == 0) {
-                            echo "Sorry, your file was not uploaded.";
+                            // echo "Sorry, your file was not uploaded.";
                         // if everything is ok, try to upload file
                         } else {
                             if (move_uploaded_file($_FILES["pictureUpload"]["tmp_name"], $target_file)) {
-                                echo "The file ". htmlspecialchars( basename( $_FILES["pictureUpload"]["name"])). " has been uploaded.";
+                                // echo "The file ". htmlspecialchars( basename( $_FILES["pictureUpload"]["name"])). " has been uploaded.";
                             } else {
-                                echo "Sorry, there was an error uploading your file.";
+                                // echo "Sorry, there was an error uploading your file.";
                             }
                         }
                     }
