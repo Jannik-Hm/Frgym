c9e60c98cfb849ceb1e533ed28bd4c0b3e24bdd9
Agent-Husky <agent-husky@gmx.de>
2022-02-02 23:10:03 +0100

    reworked new document page

--- dokumente/new-index.php
+++ dokumente/new-index.php
@@ -16,29 +16,48 @@
 
         ?>
         <section>
-            <!-- TODO: Add Back/Directory up button -->
             <?php
             if(isset($_GET["dir"])){
                 $dir = "/".$_GET["dir"];
             }else{
                 $dir = "/";
             }
+            $scriptpath = "https://".$_SERVER['SERVER_NAME'].str_replace("?".$_SERVER['QUERY_STRING'],'', $_SERVER['REQUEST_URI']);
             $root = realpath($_SERVER["DOCUMENT_ROOT"]);
             $path = "$root/files/document-page".$dir;
             $files = array_diff(scandir($path), array('.', '..'));
-            $dirup = pathinfo($path, PATHINFO_DIRNAME)."<br>"; // Path of one directory up for Back button
-            echo $dirup;
+            $dirup = pathinfo($dir, PATHINFO_DIRNAME); // Path of one directory up for Back button
+            echo("<ul style='list-style-type: none;'>");
+            // TODO: Style Back/Directory up button
+            echo("<li><div onclick='window.location=\"".$scriptpath."?dir=".$dirup."\"' class='dirup'>
+                <p>Zurück</p>
+            </div></li>");
             foreach($files as $i){
+                echo('<span class="line"></span>');
                 if (is_dir($path."/".$i)) { // Check if object is a directory
-                    echo($i."folder"); // TODO: Add href link or onclick redirect to directory
+                    echo("<li><div class='folder'>
+                        <p>".$i."folder"."</p>
+                    </div></li>");
+                    // TODO: Add href link or onclick redirect to directory
                 }elseif (is_file($path."/".$i)){ // Check if object is a file
-                    echo($i); // TODO: Add href link or onclick redirect to online preview
+                    $extension = pathinfo($i, PATHINFO_EXTENSION);
+                    if ($extension=="jpg" || $extension=="jpeg" || $extension=="png"){
+                        $is_image = true; // TODO: Create Image preview popup
+                        $previewaction = '$("#preview'.pathinfo($i, PATHINFO_FILENAME).').show()"';
+                    } else {
+                        $is_image = false; // TODO: Create redirect to online file preview
+                        $previewaction = '';
+                    }
+                    echo("<li><div onclick='".$previewaction."' class='file'>
+                        <p>".pathinfo($i, PATHINFO_FILENAME)."</p>
+                    </div></li>");
+                    // TODO: Add href link or onclick redirect to online preview
                 }else {
                     echo("unknown type");
                 }
             }
+            echo("</ul>");
         ?>  <!-- TODO: Add buttons for files and folders -->
         </section>
         <?php include_once "$root/sites/footer.html" ?>
     </body>
-</html>
