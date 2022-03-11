e24e4a9c78793bcc6c9302c21b4fe77650ff69bd
Agent-Husky <agent-husky@gmx.de>
2022-03-11 22:15:58 +0100

    slight changes to document table index

--- dokumente/new-tableindex.php
+++ dokumente/new-tableindex.php
@@ -21,11 +21,11 @@
             <!-- <iframe title="Dateivorschau" src="" id="filepreviewiframe" width="100%" style="height: 1000px; display: block"></iframe> -->
             <!-- TODO: Back / Hide Button or iframe as popup? -->
             <!-- <button onclick="$('#previewiframe').hide(); ></button>" -->
-            <div onclick="event.stopPropagation();$('.filepreview').hide();$('#files').show()" style='left: 0;' class='filepreview'>
+            <div onclick="event.stopPropagation();$('.filepreview').hide()" style='left: 0;' class='filepreview'>
                 <span class='helper'></span>
                 <div>
                     <iframe title="Dateivorschau" src="" onclick="event.stopPropagation();" id="filepreviewiframe" style="margin:auto; display:block"></iframe>
-                    <div onclick="event.stopPropagation();$('.filepreview').hide();$('#files').show()" class='popupCloseButton'>&times;</div>
+                    <div onclick="event.stopPropagation();$('.filepreview').hide();" class='popupCloseButton'>&times;</div>
                 </div>
             </div>
         </section>
@@ -60,7 +60,7 @@
                         if($dir != ""){
                             if(substr(pathinfo($dir, PATHINFO_DIRNAME), 0, 1) == "/"){ $dir = (ltrim(pathinfo($dir, PATHINFO_DIRNAME), "/")); }
                             if($dir != ""){$dir="?dir=".$dir;}
-                            echo("<tr onclick='window.location=\"".$scriptpath.$dir."\";' class='dirup'>
+                            echo("<tr onclick='window.location=\"".$scriptpath.$dir."\";' class='dirup' title='Zurück'>
                                 <td colspan='2'>
                                 <p><i class='fas fa-chevron-left' style='margin-right: 5px;'></i>Zurück</p>
                                 </td>
@@ -69,7 +69,7 @@
                         foreach($files as $i){
                             // echo('<span class="line"></span>');
                             if (is_dir($path."/".$i)) { // Check if object is a directory
-                                echo("<tr onclick='window.location.href=\"".$dirpath.$i."\";' class='folder'>
+                                echo("<tr onclick='window.location.href=\"".$dirpath.$i."\";' class='folder' title='Ordner öffnen'>
                                     <td colspan='2'>
                                     <p>
                                     <i class='far fa-folder'></i>
@@ -83,18 +83,20 @@
                                     $icon = "far fa-file-image";
                                     $is_image = true;
                                     $previewaction = 'document.getElementById("imgpreviewsrc").src="'.$pathworoot."/".$i.'";$(".img").show();';
+                                    $title = "Bild öffnen";
                                 } else if ($extension == "pdf") {
                                     $icon = "far fa-file-pdf";
                                     $is_image = false;
                                     // $previewaction = 'window.location="'."https://".$_SERVER['SERVER_NAME'].$pathworoot."/".$i.'"';
-                                    $previewaction = '$("#filepreviewiframe").attr("src", "'.$pathworoot."/".$i.'"); $("#filepreviewiframe").attr("title", "'.$i.'"); $("#files").hide(); $("#previewiframe").show();$(".filepreview").show()';
+                                    $previewaction = '$("#filepreviewiframe").attr("src", "'.$pathworoot."/".$i.'"); $("#filepreviewiframe").attr("title", "'.$i.'"); $("#previewiframe").show();$(".filepreview").show()';
+                                    $title = "PDF öffnen";
                                 } else {
                                     $icon = "far fa-file-alt";
                                     $is_image = false;
                                     $previewaction = '';
                                     $style = "cursor: default;";
                                 }
-                                echo("<tr onclick='".$previewaction."' class='file' style='".$style."'>
+                                echo("<tr onclick='".$previewaction."' class='file' style='".$style."' title='".$title."'>
                                     <td class='filename'>
                                         <p><i class='".$icon."'></i>
                                         <span class='file_name_span'>".pathinfo($i, PATHINFO_FILENAME)."</span></p>
@@ -121,7 +123,12 @@
                     listfiles($dir, $scriptpath);
                 ?>
                 <script>function onresizefunc() {
-                    if ($(".filename").width()>=7/8*$(".file_name_span").width()) { $(".editdate").show(); } else { $(".editdate").hide(); }
+                    if ($(".filename").width()>=7/8*$(".file_name_span").width() && $(window).width() > 500) { $(".editdate").show(); } else { $(".editdate").hide(); }
+                    // if (!$(".editdate").is("hidden")) {
+                    //     if ($(".filename").outerWidth()+$(".floatright").outerWidth() <= 0.9 * $(window).width()) {
+                    //         $(".editdate").hide();
+                    //     }
+                    // }
                     // if (1/2*$(window).width()>$(".file_name_span").width()) { $(".editdate").show(); } else { $(".editdate").hide(); } // TODO: implement this
                     // if ( $(".filename").outerWidth()+30+320 > $("#fileTable").width() ) { $(".editdate").hide(); } else { $(".editdate").show(); }
                 }</script>
