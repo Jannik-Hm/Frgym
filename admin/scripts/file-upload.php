40bf0048f9c857a0d09a4b1d347c36a1d30d064c
Agent-Husky <agent-husky@gmx.de>
2022-03-20 20:30:29 +0100

    moved file-upload and files-list to separate callable scripts

---
+++ admin/scripts/file-upload.php
@@ -0,0 +1,141 @@
+<?php
+
+    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+    require_once "$root/admin/scripts/admin-scripts.php";
+    verifylogin();
+
+    function dropzone($inputname) {
+        echo '<form id="file_upload" enctype="multipart/form-data" method="POST">
+        <input type="file" name="'.$inputname.'" id="'.$inputname.'" multiple hidden>
+        <div id="drop_zone" ondrop="dropHandler(event);" ondragover="dragOverHandler(event);" style="">
+            <div style="display: none" onclick="event.stopPropagation();resetupload();" class="popupCloseButton">&times;</div>
+            <p>Datei hochladen</p>
+        </div>
+        <!-- <div id="submitbtn" onclick=\'$("#file_upload").submit()\' style="display:none">
+            <p>
+            </p>
+        </div> -->
+        <input id="submitbtn" type="submit" name="submit" style="display:none">
+        <script>
+            const dropzone = $("#drop_zone")
+                // click input file field
+                dropzone.on(\'click\', function () {
+                $("#'.$inputname.'").trigger("click");
+                })
+
+                // prevent default browser behavior
+                dropzone.on("drag dragstart dragend dragover dragenter dragleave drop", function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                })
+
+                // add visual drag information
+                    dropzone.on("dragover", function() {
+                        $("#drop_zone").attr("style", "background-color: #676565");
+                    })
+                    dropzone.on("dragleave", function() {
+                        $("#drop_zone").attr("style", "background-color: ");
+                    })
+
+                function resetupload() {
+                    $("#submitbtn").hide();
+                    $("#drop_zone .popupCloseButton").hide();
+                    $("#drop_zone p").html("Datei hochladen");
+                    $("#'.$inputname.'").val("");
+                }
+
+                function onupload() {
+                    var filenames = {};
+                    for (var i = 0; i<$("#'.$inputname.'")[0].files.length; ++i) {
+                        filenames[i] = $("#'.$inputname.'")[0].files[i].name;
+                    }
+                    // function transferfiles(value, index) {filenames[index] = value;}
+                    // $("#'.$inputname.'")[0].files.forEach (transferfiles(value, index));
+                    // var filename = $("#'.$inputname.'")[0].files[0].name;
+                    // var filetype = $("#'.$inputname.'")[0].files[0].name.substr($("#'.$inputname.'")[0].files[0].name.lastIndexOf(".")+1);
+                    var filenames_string = "";
+                    for (const element in filenames) {
+                        if (element != 0) {filenames_string += " & ";}
+                        filenames_string += filenames[element].replace("."+filenames[element].substr(filenames[element].lastIndexOf(".")+1), "");
+                    }
+                    $("#drop_zone p").html(filenames_string);
+                    // TODO: change dropzone background to signalise files were added and add icons
+                    $("#submitbtn").attr("value",$("#'.$inputname.'")[0].files.length+" Datei/en freigeben");
+                    $("#submitbtn").show();
+                    $("#drop_zone .popupCloseButton").show();
+                }
+
+                // catch file drop and add it to input
+                dropzone.on("drop", e => {
+                e.preventDefault();
+                let files = e.originalEvent.dataTransfer.files
+
+                if (files.length) {
+                    $("#'.$inputname.'").prop("files", files);
+                    onupload();
+                    // document.getElementById("file_upload").submit();
+                }
+                });
+
+                // trigger file submission behavior
+                $("#'.$inputname.'").on("change", function (e) {
+                if (e.target.files.length) {
+                    onupload();
+                    // document.getElementById("file_upload").submit();
+                }
+                })
+
+                function dragOverHandler(ev) {
+                    // console.log("File(s) in drop zone");
+
+                    // Prevent default behavior (Prevent file from being opened)
+                    ev.preventDefault();
+                }
+        </script>
+        <!-- <input type="submit" name="submit" style="width: 200px; margin-left: 20px; height: auto" value="Datei freigeben"> -->
+        <!-- TODO: add upload button with "onclick=\'$("#file_upload").submit()\'" -->
+    </form>';
+    }
+
+    function uploadfile($dir, $accepted_files, $inputname, $admin) {
+        $target_dir = "/usr/www/users/greenyr/frgym/new/files/".$dir;
+        $max_filesize = 10000000;
+
+        if(!($_FILES[$inputname]["error"] == 4)) {
+            if($dir != ""){$target_dir=$target_dir."/";}
+            $extension = strtolower(pathinfo(basename($_FILES[$inputname]["name"]),PATHINFO_EXTENSION));
+            $targetfilename = basename($_FILES[$inputname]["name"]);
+            $target_file = $target_dir . $targetfilename;
+            $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            $uploadOk = 1;
+
+            // Check file size
+            if ($_FILES[$inputname]["size"] > $max_filesize) {
+                // echo "Sorry, your file is too large.";
+                $uploadOk = 0;
+            }
+
+            // Allow certain file formats
+            foreach($accepted_files as $accepted_type) {
+                if ($FileType != $accepted_type){
+                    $uploadOk = 0;
+                }else{
+                    $uploadOk = 1;
+                    break;
+                }
+            }
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                return false;
+            // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES[$inputname]["tmp_name"], $target_file)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+    }
+
---
+++ admin/scripts/list-files.php
@@ -0,0 +1,116 @@
+<link rel="stylesheet" href="/css/documentstable2.css">
+<section id="previewiframe" style="display: none">
+    <div onclick="event.stopPropagation();$('.filepreview').hide()" style='left: 0;' class='filepreview'>
+        <span class='helper'></span>
+        <div>
+            <iframe title="Dateivorschau" src="" onclick="event.stopPropagation();" id="filepreviewiframe" style="margin:auto; display:block"></iframe>
+            <div onclick="event.stopPropagation();$('.filepreview').hide();" class='popupCloseButton'>&times;</div>
+        </div>
+    </div>
+</section>
+<section id="files">
+    <table id="fileTable">
+    <tr style="display: none">
+        <td></td>
+        <td></td>
+    </tr>
+
+        <?php
+            if(isset($_GET["dir"])){
+                $dir = "/".$_GET["dir"];
+            }else{
+                $dir = "";
+            }
+            $scriptpath = "https://".$_SERVER['SERVER_NAME'].str_replace("?".$_SERVER['QUERY_STRING'],'', $_SERVER['REQUEST_URI']);
+            function changedir($url) {
+                echo("window.history.pushState({}, null, \"".$url."\")");
+            }
+            function listfiles($dir, $scriptpath) {
+                $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+                if($_SERVER['QUERY_STRING'] != ""){
+                    $dirpath = "https://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."/";
+                }else{
+                    $dirpath = "https://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."?dir=";
+                }
+                $pathworoot = "/files/document-page".$dir;
+                $path = $root.$pathworoot;
+                $files = array_diff(scandir($path), array('.', '..'));
+                // echo("<ul class='docs-list' style='list-style-type: none'>");
+                if($dir != ""){
+                    if(substr(pathinfo($dir, PATHINFO_DIRNAME), 0, 1) == "/"){ $dir = (ltrim(pathinfo($dir, PATHINFO_DIRNAME), "/")); }
+                    if($dir != ""){$dir="?dir=".$dir;}
+                    echo("<tr onclick='window.location=\"".$scriptpath.$dir."\";' class='dirup' title='Zurück'>
+                        <td colspan='2'>
+                        <p><i class='fas fa-chevron-left' style='margin-right: 5px;'></i>Zurück</p>
+                        </td>
+                        </tr>"); // window.history.pushState({}, null, \"".$scriptpath.$dir."\")
+                }else{$hidefirstline = "<style>span.line:first-child { display: none; } </style>";}
+                foreach($files as $i){
+                    // echo('<span class="line"></span>');
+                    if (is_dir($path."/".$i)) { // Check if object is a directory
+                        echo("<tr onclick='window.location.href=\"".$dirpath.$i."\";' class='folder' title='Ordner öffnen'>
+                            <td colspan='2'>
+                            <p>
+                            <i class='far fa-folder'></i>
+                            <span class='file_name_span'>".$i."</span>
+                            </p>
+                            </td>
+                            </tr>");
+                    }elseif (is_file($path."/".$i)){ // Check if object is a file
+                        $extension = pathinfo($i, PATHINFO_EXTENSION);
+                        if ($extension=="jpg" || $extension=="jpeg" || $extension=="png"){
+                            $icon = "far fa-file-image";
+                            $is_image = true;
+                            $previewaction = 'document.getElementById("imgpreviewsrc").src="'.$pathworoot."/".$i.'";$(".img").show();';
+                            $title = "Bild öffnen";
+                        } else if ($extension == "pdf") {
+                            $icon = "far fa-file-pdf";
+                            $is_image = false;
+                            // $previewaction = 'window.location="'."https://".$_SERVER['SERVER_NAME'].$pathworoot."/".$i.'"';
+                            $previewaction = '$("#filepreviewiframe").attr("src", "'.$pathworoot."/".$i.'"); $("#filepreviewiframe").attr("title", "'.$i.'"); $("#previewiframe").show();$(".filepreview").show()';
+                            $title = "PDF öffnen";
+                        } else {
+                            $icon = "far fa-file-alt";
+                            $is_image = false;
+                            $previewaction = '';
+                            $style = "cursor: default;";
+                            $title = null;
+                        }
+                        echo("<tr onclick='".$previewaction."' class='file' style='".$style."' title='".$title."'>
+                            <td class='filename'>
+                                <p><i class='".$icon."'></i>
+                                <span class='file_name_span'>".pathinfo($i, PATHINFO_FILENAME)."</span></p>
+                            </td>
+                            <td class='floatright' nowrap='nowrap'>
+                                <p><a class='downloadlink' href='".$pathworoot."/".$i."' onclick=\"event.stopPropagation();\" download><i class='far fa-save download' title='Herunterladen'></i></a>
+                                <span class='editdate'>Hochgeladen am: ".date("d.m.Y H:i:s", filemtime($path."/".$i))."</span></p>
+                            </td>
+                        </tr>");
+                    }else {
+                        echo("unknown type");
+                    }
+                }
+                echo("<div onclick=\"event.stopPropagation();$('.img').hide()\" style='left: 0;' class='img'>
+                    <span class='helper'></span>
+                    <div>
+                        <img onclick=\"event.stopPropagation();\" id='imgpreviewsrc' style='margin: auto'>
+                        <div onclick=\"event.stopPropagation();$('.img').hide()\" class='popupCloseButton'>&times;</div>
+                    </div>
+                </div>");
+                echo($hidefirstline);
+                echo("</ul>");
+            }
+            listfiles($dir, $scriptpath);
+        ?>
+        <script>function onresizefunc() {
+            if ($(".filename").width()>=7/8*$(".file_name_span").width() && $(window).width() > 500) { $(".editdate").show(); } else { $(".editdate").hide(); }
+            // if (!$(".editdate").is("hidden")) {
+            //     if ($(".filename").outerWidth()+$(".floatright").outerWidth() <= 0.9 * $(window).width()) {
+            //         $(".editdate").hide();
+            //     }
+            // }
+            // if (1/2*$(window).width()>$(".file_name_span").width()) { $(".editdate").show(); } else { $(".editdate").hide(); } // TODO: implement this
+            // if ( $(".filename").outerWidth()+30+320 > $("#fileTable").width() ) { $(".editdate").hide(); } else { $(".editdate").show(); }
+        }</script>
+    </table>
+</section>
