9cac858d3c837a90397319871c425593c472c7ec
Agent-Husky <agent-husky@gmx.de>
2022-03-21 15:57:28 +0100

    added boolean return to verifylogin scripts

--- admin/scripts/admin-scripts.php
+++ admin/scripts/admin-scripts.php
@@ -7,6 +7,8 @@
         if(!isset($_SESSION["user_id"])) {
             // header("Location: /admin/login/");
             echo "<script>window.location.replace('/admin/login/')</script>";
+        }else{
+            return true;
         }
     }
 
