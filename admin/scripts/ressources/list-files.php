a7839a8ffa178da691b8c0ce3649366ff1f790b0
Agent-Husky <agent-husky@gmx.de>
2022-04-07 12:32:46 +0200

    renamed documentstable2 to documents.css

--- admin/scripts/ressources/list-files.php
+++ admin/scripts/ressources/list-files.php
@@ -1,5 +1,5 @@
 <section id="previewiframe" style="display: none">
-    <link rel="stylesheet" href="/css/documentstable2.css">
+    <link rel="stylesheet" href="/css/documents.css">
     <div onclick="event.stopPropagation();$('.filepreview').hide()" style='left: 0;' class='filepreview'>
         <span class='helper'></span>
         <div>
