7101811f986d030ac556dc2291c5fe7014081108
Agent-Husky <agent-husky@gmx.de>
2022-06-18 21:53:55 +0200

    added source to every faecher icon

--- faecher/index.php
+++ faecher/index.php
@@ -14,58 +14,57 @@
                 <div class="bereichdiv">
                     <label class="fachbereich">Sprachwissenschaften</label><br>
                     <ul class="faecherlist">
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Deutsch.svg" class="fachimg"><br><span>Deutsch</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Englisch.svg" class="fachimg"><br><span>Englisch</span></div></li></a>
-                        <a><li class="fach"><div><img src="https://upload.wikimedia.org/wikipedia/commons/0/01/France_flag_waving_icon.svg" class="fachimg"><br><span>Französisch</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Deutsch.svg" class="fachimg"><br><span>Deutsch</span></div></li></a> <!-- https://freesvg.org/stack-of-books-vector-clip-art -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Englisch.svg" class="fachimg"><br><span>Englisch</span></div></li></a> <!-- https://en.wikipedia.org/wiki/File:English_language.svg -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Französisch.svg" class="fachimg"><br><span>Französisch</span></div></li></a> <!-- https://commons.wikimedia.org/wiki/File:France_flag_waving_icon.svg -->
                         <!-- <a><li class="fach"><div><img src="" class="fachimg"><br><span>Polnisch</span></div></li></a> -->
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Russisch.svg" class="fachimg"><br><span>Russisch</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Russisch.svg" class="fachimg"><br><span>Russisch</span></div></li></a> <!-- https://freesvg.org/flag-of-the-russian-federation-vector-graphics -->
                         <!-- <a><li class="fach"><div><img src="" class="fachimg"><br><span>Spanisch</span></div></li></a> -->
                         <!-- <a><li class="fach"><div><img src="" class="fachimg"><br><span>Türkisch</span></div></li></a> -->
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Latein.svg" class="fachimg"><br><span>Latein</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Latein.svg" class="fachimg"><br><span>Latein</span></div></li></a> <!-- https://svgsilh.com/image/161704.html -->
                     </ul>
                 </div>
                 <div class="bereichdiv">
                     <label class="fachbereich">Naturwissenschaften</label><br>
                     <ul class="faecherlist">
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Mathematik.svg" class="fachimg"><br><span>Mathematik</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Biologie.svg" class="fachimg"><br><span>Biologie</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Chemie.svg" class="fachimg"><br><span>Chemie</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Physik.svg" class="fachimg"><br><span>Physik</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Informatik.svg" class="fachimg"><br><span>Informatik</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Naturwissenschaften.svg" class="fachimg"><br><span>Naturwissenschaften</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Mathematik.svg" class="fachimg"><br><span>Mathematik</span></div></li></a> <!-- https://de.wikipedia.org/wiki/Datei:Math.svg -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Biologie.svg" class="fachimg"><br><span>Biologie</span></div></li></a> <!-- https://www.labbe.de/blog/Ruth-Patrick -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Chemie.svg" class="fachimg"><br><span>Chemie</span></div></li></a> <!-- https://svgsilh.com/de/image/1073363.html -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Physik.svg" class="fachimg"><br><span>Physik</span></div></li></a> <!-- https://svgsilh.com/de/image/148441.html -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Informatik.svg" class="fachimg"><br><span>Informatik</span></div></li></a> <!-- https://www.svgrepo.com/svg/28224/programming -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Naturwissenschaften.svg" class="fachimg"><br><span>Naturwissenschaften</span></div></li></a> <!-- https://svgsilh.com/image/2099128.html -->
                     </ul>
                 </div>
                 <div class="bereichdiv">
                     <label class="fachbereich">Gesellschaftswissenschaften</label><br>
                     <ul class="faecherlist">
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Erdkunde.svg" class="fachimg"><br><span>Erdkunde</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Geschichte.svg" class="fachimg"><br><span>Geschichte</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/PB.svg" class="fachimg"><br><span>Politische Bildung</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Gesellschaftswissenschaften.svg" class="fachimg"><br><span>Gesellschaftswissenschaften</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/RE.svg" class="fachimg"><br><span>Religion</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Erdkunde.svg" class="fachimg"><br><span>Erdkunde</span></div></li></a> <!-- https://svgsilh.com/image/158348.html -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Geschichte.svg" class="fachimg"><br><span>Geschichte</span></div></li></a> <!-- https://svgsilh.com/image/33963.html -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/PB.svg" class="fachimg"><br><span>Politische Bildung</span></div></li></a> <!-- https://svgsilh.com/image/32310.html -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Gesellschaftswissenschaften.svg" class="fachimg"><br><span>Gesellschaftswissenschaften</span></div></li></a> <!--  -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/RE.svg" class="fachimg"><br><span>Religion</span></div></li></a> <!--  -->
                         <!-- <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Eva_RE.svg" class="fachimg"><br><span>Evangelischer Religionsunterricht</span></div></li></a>
                         <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Kat_RE.svg" class="fachimg"><br><span>Katholischer Religionsunterricht</span></div></li></a> -->
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/LER.svg" class="fachimg"><br><span>Lebensgestaltung-Ethik-Religionskunde</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/WAT.svg" class="fachimg"><br><span>Wirtschaft-Arbeit-Technik<br>Wirtschaftswissenschaften</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/LER.svg" class="fachimg"><br><span>Lebensgestaltung-Ethik-Religionskunde</span></div></li></a> <!--  -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/WAT.svg" class="fachimg"><br><span>Wirtschaft-Arbeit-Technik<br>Wirtschaftswissenschaften</span></div></li></a> <!--  -->
                         <!-- <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Wirtschaft.svg" class="fachimg"><br><span>Wirtschaftswissenschaften</span></div></li></a> -->
                     </ul>
                 </div>
                 <div class="bereichdiv">
                     <label class="fachbereich">Künstlerische Fächer</label><br>
                     <ul class="faecherlist">
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/DS.svg" class="fachimg"><br><span>Darstellendes Spiel</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Kunst.svg" class="fachimg"><br><span>Kunst</span></div></li></a>
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Musik.svg" class="fachimg"><br><span>Musik</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/DS.svg" class="fachimg"><br><span>Darstellendes Spiel</span></div></li></a> <!-- https://www.ardenne-gymnasium.de/media/icon_darstellendes_spiel.svg -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Kunst.svg" class="fachimg"><br><span>Kunst</span></div></li></a> <!-- https://publicdomainvectors.org/de/kostenlose-vektorgrafiken/Pinsel-Paletten/40255.html -->
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Musik.svg" class="fachimg"><br><span>Musik</span></div></li></a> <!-- https://svgsilh.com/image/2073069.html -->
                     </ul>
                 </div>
                 <div class="bereichdiv">
                     <label class="fachbereich">Sonstige</label><br>
                     <ul class="faecherlist">
-                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Sport.svg" class="fachimg"><br><span>Sport</span></div></li></a>
+                        <a><li class="fach"><div><img src="/files/site-ressources/faecher-icons/Sport.svg" class="fachimg"><br><span>Sport</span></div></li></a> <!-- https://svgsilh.com/image/309539.html -->
                     </ul>
                 </div>
             </section>
-
             <?php include_once realpath($_SERVER["DOCUMENT_ROOT"])."/sites/footer.html" ?>
     </body>
 </html>
