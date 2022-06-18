7c5f8186ba187d23434ee0ff4b35886c4fe18256
Agent-Husky <agent-husky@gmx.de>
2022-06-18 15:40:38 +0200

    beginning of faecher page

---
+++ faecher/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="de-DE" prefix="og: https://ogp.me/ns#" xmlns:og="http://opengraphprotocol.org/schema/">
+    <head>
+        <?php
+            include_once realpath($_SERVER["DOCUMENT_ROOT"])."/sites/head.html"
+        ?>
+        <title>Fächer - Friedrich-Gymnasium Luckenwalde</title>
+        <link rel="stylesheet" href="/new-css/faecher.css">
+    </head>
+    <body>
+            <?php include_once realpath($_SERVER["DOCUMENT_ROOT"])."/sites/header.html" ?>
+
+            <section class="faecher">
+                <div>
+                    <label class="fachbereich">Sprachwissenschaften</label><br>
+                    <ul class="faecherlist">
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Deutsch</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Englisch</span></li></a>
+                        <a><li class="fach"><img src="https://upload.wikimedia.org/wikipedia/commons/0/01/France_flag_waving_icon.svg" class="fachimg"><br><span>Französisch</span></li></a>
+                        <!-- <a><li class="fach"><img src="" class="fachimg"><br><span>Polnisch</span></li></a> -->
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Russisch</span></li></a>
+                        <!-- <a><li class="fach"><img src="" class="fachimg"><br><span>Spanisch</span></li></a> -->
+                        <!-- <a><li class="fach"><img src="" class="fachimg"><br><span>Türkisch</span></li></a> -->
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Latein</span></li></a>
+                    </ul>
+                </div>
+                <div>
+                    <label class="fachbereich">Naturwissenschaften</label><br>
+                    <ul class="faecherlist">
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Mathematik</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Biologie</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Chemie</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Physik</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Informatik</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Naturwissenschaften</span></li></a>
+                    </ul>
+                </div>
+                <div>
+                    <label class="fachbereich">Gesellschaftswissenschaften</label><br>
+                    <ul class="faecherlist">
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Erdkunde</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Geschichte</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Politische Bildung</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Gesellschaftswissenschaften</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Evangelischer Religionsunterricht</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Katholischer Religionsunterricht</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Lebensgestaltung-Ethik-Religionskunde</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Wirtschat-Arbeit-Technik</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Wirtschaftswissenschaften</span></li></a>
+                    </ul>
+                </div>
+                <div>
+                    <label class="fachbereich">Künstlerische Fächer</label><br>
+                    <ul class="faecherlist">
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Darstellendes Spiel</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Kunst</span></li></a>
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Musik</span></li></a>
+                    </ul>
+                </div>
+                <div>
+                    <label class="fachbereich">Sonstige</label><br>
+                    <ul class="faecherlist">
+                        <a><li class="fach"><img src="" class="fachimg"><br><span>Sport</span></li></a>
+                    </ul>
+                </div>
+            </section>
+
+            <?php include_once realpath($_SERVER["DOCUMENT_ROOT"])."/sites/footer.html" ?>
+    </body>
+</html>
