e13954528e0cf94293923a9091b94798cf0896c0
Agent-Husky <agent-husky@gmx.de>
2022-09-02 20:27:48 +0200

    added href to faecher page if visibility is set

--- faecher/index.php
+++ faecher/index.php
@@ -8,7 +8,17 @@
         <link rel="stylesheet" href="/new-css/faecher.css">
     </head>
     <body>
-            <?php include_once realpath($_SERVER["DOCUMENT_ROOT"])."/sites/header.html" ?>
+            <?php
+            include_once realpath($_SERVER["DOCUMENT_ROOT"])."/sites/header.html";
+            require_once realpath($_SERVER["DOCUMENT_ROOT"])."/admin/scripts/admin-scripts.php";
+            $result = mysqli_query(getsqlconnection(), "SELECT fach, content1 FROM faecher WHERE contenttype='visibility'");
+            $visibilitylist = array();
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()){
+                    $visibilitylist[$row["fach"]] = $row["content1"];
+                }
+            }
+            ?>
             <!-- TODO: add hrefs to faecher pages + missing icons -->
             <section class="faecher">
                 <?php
@@ -19,8 +29,9 @@
                             <label class="fachbereich">'.$fachbereich["name"].'</label><br>
                             <ul class="faecherlist">';
                             foreach($fachbereich["faecher"] as $fach){
+                                if($visibilitylist[$fach["name"]] == "visible"){$link = "href='".$_SERVER['PHP_SELF']."?fach=".$fach["name"]."'";}else{$link = NULL;}
                                 if($fach["dark-image"]){$darkpath = '<source srcset="/files/site-ressources/faecher-icons/'.$fach["filename"].'-Dark.svg" media="(prefers-color-scheme: dark)">';}else{$darkpath = NULL;}
-                                echo '<a><li class="fach"><figcaption>i</figcaption><object><a href="'.$fach["pic-url"].'" class="imgsource">'.$fach["pic-url"].'</a class="imgsource"></object><div><picture>'.$darkpath.'<img src="/files/site-ressources/faecher-icons/'.$fach["filename"].'.svg" class="fachimg"></picture><br><span>'.$fach["name"].'</span></div></li></a>';
+                                echo '<a '.$link.'><li class="fach"><figcaption>i</figcaption><object><a href="'.$fach["pic-url"].'" class="imgsource">'.$fach["pic-url"].'</a class="imgsource"></object><div><picture>'.$darkpath.'<img src="/files/site-ressources/faecher-icons/'.$fach["filename"].'.svg" class="fachimg"></picture><br><span>'.$fach["name"].'</span></div></li></a>';
                             }
                         echo '
                             </ul>
