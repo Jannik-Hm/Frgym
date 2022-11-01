85c230ef03766cddd5a633ed0fc8c65215a3b3cc
Agent-Husky <agent-husky@gmx.de>
2022-11-01 20:57:11 +0100

    finished up news api

--- admin/api/news.php
+++ admin/api/news.php
@@ -5,23 +5,35 @@
     $password = $_POST["password_hash"];
     $incadmin = filter_var($_POST["includeadmins"], FILTER_VALIDATE_BOOLEAN);
     $id = $_POST["id"];
-    if($app == "get"){
+    if($app == "getall"){
         $response["performed"] = "getall";
-        $select = mysqli_query(getsqlconnection(), "SELECT * FROM news ORDER BY zeit DESC".((is_null($_POST["limit"]) || $_POST["limit"] == "") ? "" : (" LIMIT ".$_POST["limit"])));
-        $response["data"] = [];
+        $select = mysqli_query(getsqlconnection(), "SELECT news.id, news.titel, news.inhalt, news.zeit, CONCAT(if(users.titel IS NOT NULL, CONCAT(users.titel, ' '), ''), users.vorname,' ',users.nachname) AS autor FROM news, users WHERE news.autor = users.id ORDER BY zeit DESC".((is_null($_POST["limit"]) || $_POST["limit"] == "") ? "" : (" LIMIT ".$_POST["limit"])));
+        $response["data"] = array();
         while ($db_field = mysqli_fetch_assoc($select)) {
-            array_push($response["data"], $db_field);
+            $response["data"][] = $db_field;
+        }
+    }elseif($app == "getbyid"){
+        $response["performed"] = "getbyid";
+        if(isset($_POST["id"])){
+            $conn = getsqlconnection();
+            $sql = $conn->prepare("SELECT news.id, news.titel, news.inhalt, news.zeit, CONCAT(if(users.titel IS NOT NULL, CONCAT(users.titel, ' '), ''), users.vorname,' ',users.nachname) AS autor FROM news, users WHERE news.autor = users.id AND news.id=?");
+            $sql->bind_param("s", $_POST["id"]);
+            $sql->execute();
+            $db_field = mysqli_fetch_assoc($sql->get_result());
+            $response["data"] = $db_field;
+        }else{
+            $response["error"] = "Missing id";
         }
     }elseif($app == "add"){
         $user = verifyapi($username, $password);
         if(!is_array($user)){
             $response["error"] = $user;
         }else{
-            if($user["perms"]["news.all"] == 1 || ($user["perms"]["news.own"] == 1 && $user["vorname"]." ".$user["nachname"] == $autor)){
+            if($user["perms"]["news.all"] == 1 || $user["perms"]["news.own"] == 1 ){
                 $response["performed"] = "add news";
                 $conn = getsqlconnection();
-                $sql = $conn->prepare("");
-                $sql->bind_param("s", $id);
+                $sql = $conn->prepare("INSERT INTO news (autor, titel, inhalt, zeit) VALUES (?, ?, ?, ?)");
+                $sql->bind_param("ssss", $user["id"], $_POST["titel"], $_POST["inhalt"], date("Y-m-d H:i"));
                 $sql->execute();
                 if($sql->affected_rows != 0){
                     $response["success"] = true;
@@ -37,12 +49,16 @@
         if(!is_array($user)){
             $response["error"] = $user;
         }else{
-            if($user["perms"]["news.all"] == 1 || ($user["perms"]["news.own"] == 1 && $user["vorname"]." ".$user["nachname"] == $autor)){
+            $conn = getsqlconnection();
+            $sql = $conn->prepare("SELECT autor FROM news WHERE news.id=?");
+            $sql->bind_param("s", $_POST["id"]);
+            $sql->execute();
+            $autor = mysqli_fetch_assoc($sql->get_result());
+            if($user["perms"]["news.all"] == 1 || ($user["perms"]["news.own"] == 1 && $user["id"] == $autor["autor"])){
                 $response["performed"] = "update news";
                 $conn = getsqlconnection();
-                $displayname = mb_substr($_POST["vorname"], 0, 1).".";
-                $sql = $conn->prepare("");
-                $sql->bind_param("s", $bla);
+                $sql = $conn->prepare("UPDATE news SET autor=?, titel=?, inhalt=?, zeit=? WHERE id=?");
+                $sql->bind_param("sssss", $user["id"], $_POST["titel"], $_POST["inhalt"], date("Y-m-d H:i"), $_POST["id"]);
                 $sql->execute();
                 if($sql->affected_rows != 0){
                     $response["success"] = true;
@@ -58,10 +74,15 @@
         if(!is_array($user)){
             $response["error"] = $user;
         }else{
-            if($user["perms"]["news.all"] == 1 || ($user["perms"]["news.own"] == 1 && $user["vorname"]." ".$user["nachname"] == $autor)){
+            $conn = getsqlconnection();
+            $sql = $conn->prepare("SELECT autor FROM news WHERE news.id=?");
+            $sql->bind_param("s", $_POST["id"]);
+            $sql->execute();
+            $autor = mysqli_fetch_assoc($sql->get_result());
+            if($user["perms"]["news.all"] == 1 || ($user["perms"]["news.own"] == 1 && $user["id"] == $autor["autor"])){
                 $response["performed"] = "delete news";
                 $conn = getsqlconnection();
-                $sql = $conn->prepare("");
+                $sql = $conn->prepare("DELETE FROM news WHERE id=?");
                 $sql->bind_param("s", $id);
                 $sql->execute();
                 if($sql->affected_rows != 0){
