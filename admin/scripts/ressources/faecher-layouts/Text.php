b25201848cd3cc69244d6b9b1fffc4df0b80586d
Agent-Husky <agent-husky@gmx.de>
2022-11-28 21:01:50 +0100

    updated text fach layout to work with new api system

--- admin/scripts/ressources/faecher-layouts/Text.php
+++ admin/scripts/ressources/faecher-layouts/Text.php
@@ -1,17 +1,18 @@
 <?php
-    require_once realpath($_SERVER["DOCUMENT_ROOT"])."/admin/scripts/admin-scripts.php";
-    $result = mysqli_query(getsqlconnection(), "SELECT * FROM faecher WHERE id=\"{$GLOBALS["id"]}\"");
+    // require_once realpath($_SERVER["DOCUMENT_ROOT"])."/admin/scripts/admin-scripts.php";
+    // $result = mysqli_query(getsqlconnection(), "SELECT * FROM faecher WHERE id=\"{$GLOBALS["id"]}\"");
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
     }
-    if($GLOBALS["viewer"]){
-        echo '<p>'.nl2br($row["content1"]).'</p>';
+    if($viewer){
+        echo '<p>'.nl2br($data[0]).'</p>';
     }else{
         echo '
         <div class="grow-wrap">
-            <textarea name="content1" id="content1'.$GLOBALS["id"].'" class="normal" onInput="this.parentNode.dataset.replicatedValue = this.value" placeholder="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, nostrum autem reiciendis quidem quaerat in non laboriosam distinctio officia debitis similique laudantium ducimus doloribus numquam natus error minus eos cupiditate." disabled>'.$row["content1"].'</textarea>
+            <textarea name="content1" id="content1'.$id.'" class="normal" onInput="this.parentNode.dataset.replicatedValue = this.value" placeholder="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, nostrum autem reiciendis quidem quaerat in non laboriosam distinctio officia debitis similique laudantium ducimus doloribus numquam natus error minus eos cupiditate." disabled>'.$data[0].'</textarea>
             <input name="contenttype" type="text" value="Text" hidden></input>
         </div>
         ';
+        $savefunction .= ajaxsave("Text", '[$("#content1'.$id.'").val()]');
     }
 ?>
