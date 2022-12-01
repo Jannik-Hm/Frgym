fd1ab0790c4916d2ad5f7f31dc1ded7a6dd804bb
Agent-Husky <agent-husky@gmx.de>
2022-12-01 22:23:20 +0100

    added deletion of edit button on Absatz segment

--- admin/scripts/ressources/faecher-layouts/Absatz.php
+++ admin/scripts/ressources/faecher-layouts/Absatz.php
@@ -1,3 +1,4 @@
 <section style="height: 25px">
     <input name="contenttype" type="text" value="Absatz" hidden></input>
+    <script>$("input[value='Absatz']").parent().parent().children("div").children("[id*='edit']").remove()</script>
 </section>
