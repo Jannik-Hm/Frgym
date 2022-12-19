99926cddfb0b395e629f3808cb55ca41e1fcdeec
Agent-Husky <agent-husky@gmx.de>
2022-12-19 17:51:27 +0100

    updated rundgang page + kept old files as backup

---
+++ rundgang/index-old.php
@@ -0,0 +1,31 @@
+﻿<html>
+    <head>
+        <?php
+            include_once "./../sites/head.html";
+        ?>
+        <link rel="stylesheet" href="./style.css">
+        <title>Rundgang - Friedrich-Gymnasium Luckenwalde</title>
+    </head>
+    <body>
+        <?php
+            include_once "./../sites/header.html";
+        ?>
+        <section>
+            <h1 style="padding-bottom: 20px; padding-top: 2%">Rundgang</h1>
+            <div style="padding-bottom: 60px;">
+                <picture>
+                    <img src="./rundga1.jpg" alt="Schule von vorne" style="margin-bottom: 30px; border-radius: 30px;">
+                </picture>
+                <a href="./altbau/">Altbau</a>
+                <a href="./neubau/">Neubau</a>
+                <a href="./sporthalle/">Sporthalle</a>
+                <a href="./1910/">Schule im Jahr 1910</a>
+            </div>
+        </section>
+        <?php
+
+            include_once "./../sites/footer.html"
+
+        ?>
+    </body>
+</html>
