83cae35ba0ba007304d493a49ee56880a2b42e20
Agent-Husky <agent-husky@gmx.de>
2023-05-04 16:07:04 +0200

    added adding, editing and deleting events ability

--- admin/api/calendar.php
+++ admin/api/calendar.php
@@ -158,6 +158,7 @@
                 $tempeventarray = array();
                 $tempeventarray["name"] = trim(str_replace(["Erster", "Zweiter", "Dritter", "Vierter", "der", "Deutschen", "Neujahrstag"], ["1.", "2.", "3.", "4.", "d.", "Dt.", "Neujahr"], str_replace(["(regionaler Feiertag)", "Halloween", "St. Martin", "Volkstrauertag", "Totensonntag", "Heilige Drei Könige", "Valentinstag", "Rosenmontag", "Faschingsdienstag", "Aschermittwoch", "Palmsonntag", "Jahrestag der Befreiung vom Nationalsozialismus", "Internationaler Frauentag", "Vatertag", "Fronleichnam", "Allerheiligen", "Mariä Himmelfahrt", "Nikolaustag", "Gründonnerstag", "Karsamstag", "Muttertag"], NULL, (preg_match("(Bayern|Sachsen|Sommerzeit|Thüringen)", $entry["summary"]) === 1) ? "" : $entry["summary"])));
                 if($tempeventarray["name"] == "" || $tempeventarray["name"] == NULL) continue;
+                $tempeventarray["id"] = $entry["id"];
                 $tempeventarray["description"] = str_replace("\nWenn Sie Gedenktage ausblenden möchten, rufen Sie die Google Kalender-Einstellungen auf > Feiertage in Deutschland", "", $entry["description"]);
                 if(strpos($tempeventarray["description"], "Feiertag") !== false){
                     $tempeventarray["description"] = "Feiertag";
@@ -181,7 +182,228 @@
         return $calarray;
     }
 
+    function geteventdata($calid, $eventid){
+        $i = 0;
+        begin:
+        $i++;
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://www.googleapis.com/calendar/v3/calendars/".urlencode($calid)."/events/$eventid",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => [
+                "Authorization: ".$token_type." ".$access_token.""
+            ],
+        ]);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $result = json_decode($response, true);
+        if($result["error"]["code"] == 401) {
+            $access_token = getnewtoken($refresh_token, $client_id, $client_secret);
+            if(isset($access_token)){
+                $GLOBALS["tokens"]["readonly"]["access_token"] = $access_token;;
+                file_put_contents(realpath($_SERVER["DOCUMENT_ROOT"])."/secrets/GoogleApisecrets.json", json_encode($GLOBALS["tokens"]));
+            }
+            if($i < 2){
+                goto begin;
+            }
+            return;
+        }else{
+            return $result;
+        }
+    }
 
+    function converttogoogletime($datetime){
+        return date("Y-m-d\TH:i:sP", strtotime($datetime));
+    }
+
+    function create_event($username, $password, $calid, $title, $description, $location, $start, $end, $isdayevent){
+        $access_token = $GLOBALS["tokens"]["edit"]["access_token"];
+        $refresh_token = $GLOBALS["tokens"]["edit"]["refresh_token"];
+        $token_type = $GLOBALS["tokens"]["edit"]["token_type"];
+        $client_id = $GLOBALS["tokens"]["edit"]["client_id"];
+        $client_secret = $GLOBALS["tokens"]["edit"]["client_secret"];
+        $user = verifyapi($username, $password);
+        if(is_array($user) && $user["perms"]["calendar.administration"]){
+            $i = 0;
+            begin:
+            $i++;
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://www.googleapis.com/calendar/v3/calendars/$calid/events",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                // CURLOPT_POSTFIELDS => "{\"end\": {\"dateTime\":\"$end\"},\"start\":{\"dateTime\":\"$start\"},\"summary\":\"$summary\"}",
+                CURLOPT_HTTPHEADER => [
+                    "Authorization: ".$token_type." ".$access_token,
+                    "Content-Type: application/json"
+                ],
+            ]);
+            if($isdayevent){
+                curl_setopt($curl, CURLOPT_POSTFIELDS, "{\"end\": {\"date\":\"$end\"},\"start\":{\"date\":\"$start\"},\"summary\":\"$title\", \"description\":\"$description\", \"location\":\"$location\"}");
+            }else{
+                curl_setopt($curl, CURLOPT_POSTFIELDS, "{\"end\": {\"dateTime\":\"".converttogoogletime($end)."\"},\"start\":{\"dateTime\":\"".converttogoogletime($start)."\"},\"summary\":\"$title\", \"description\":\"$description\", \"location\":\"$location\"}");
+            }
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            $result = json_decode($response, true);
+            if($result["error"]["code"] == 401) {
+                $access_token = getnewtoken($refresh_token, $client_id, $client_secret);
+                if(isset($access_token)){
+                    $GLOBALS["tokens"]["edit"]["access_token"] = $access_token;;
+                    file_put_contents(realpath($_SERVER["DOCUMENT_ROOT"])."/secrets/GoogleApisecrets.json", json_encode($GLOBALS["tokens"]));
+                }
+                if($i < 2){
+                    goto begin;
+                }
+                return;
+            }else{
+                return $result;
+            }
+        }else{
+            $response["error"] = $user;
+            return $response;
+        }
+    }
+
+    function update_event($username, $password, $calid, $eventid, $title, $description, $location, $start, $end, $isdayevent){
+        $access_token = $GLOBALS["tokens"]["edit"]["access_token"];
+        $refresh_token = $GLOBALS["tokens"]["edit"]["refresh_token"];
+        $token_type = $GLOBALS["tokens"]["edit"]["token_type"];
+        $client_id = $GLOBALS["tokens"]["edit"]["client_id"];
+        $client_secret = $GLOBALS["tokens"]["edit"]["client_secret"];
+        $user = verifyapi($username, $password);
+        if(empty($title) || empty($description) || empty($location) || empty($start) || empty($end)){
+            $prevdata = geteventdata($calid, $eventid);
+            if(empty($title)) $title = $prevdata["summary"];
+            if(empty($description)) $description = $prevdata["description"];
+            if(empty($location)) $location = $prevdata["location"];
+            if(empty($start)){
+                if(isset($prevdata["start"]["date"])){
+                    $start = $prevdata["start"]["date"];
+                    $isdayevent = true;
+                }
+                if(isset($prevdata["start"]["dateTime"])){
+                    $start = $prevdata["start"]["dateTime"];
+                    $isdayevent = false;
+                }
+            }
+            if(empty($end)){
+                if(isset($prevdata["end"]["date"])){
+                    $end = $prevdata["end"]["date"];
+                    $isdayevent = true;
+                }
+                if(isset($prevdata["end"]["dateTime"])){
+                    $start = $prevdata["end"]["dateTime"];
+                    $isdayevent = false;
+                }
+            }
+        }
+        if(is_array($user) && $user["perms"]["calendar.administration"]){
+            $i = 0;
+            begin:
+            $i++;
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://www.googleapis.com/calendar/v3/calendars/$calid/events/$eventid",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "PUT",
+                // CURLOPT_POSTFIELDS => "{\"end\": {\"dateTime\":\"$end\"},\"start\":{\"dateTime\":\"$start\"},\"summary\":\"$summary\"}",
+                CURLOPT_HTTPHEADER => [
+                    "Authorization: ".$token_type." ".$access_token,
+                    "Content-Type: application/json"
+                ],
+            ]);
+            if($isdayevent){
+                curl_setopt($curl, CURLOPT_POSTFIELDS, "{\"end\": {\"date\":\"$end\"},\"start\":{\"date\":\"$start\"},\"summary\":\"$title\", \"description\":\"$description\", \"location\":\"$location\"}");
+            }else{
+                curl_setopt($curl, CURLOPT_POSTFIELDS, "{\"end\": {\"dateTime\":\"".converttogoogletime($end)."\"},\"start\":{\"dateTime\":\"".converttogoogletime($start)."\"},\"summary\":\"$title\", \"description\":\"$description\", \"location\":\"$location\"}");
+            }
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            $result = json_decode($response, true);
+            if($result["error"]["code"] == 401) {
+                $access_token = getnewtoken($refresh_token, $client_id, $client_secret);
+                if(isset($access_token)){
+                    $GLOBALS["tokens"]["edit"]["access_token"] = $access_token;;
+                    file_put_contents(realpath($_SERVER["DOCUMENT_ROOT"])."/secrets/GoogleApisecrets.json", json_encode($GLOBALS["tokens"]));
+                }
+                if($i < 2){
+                    goto begin;
+                }
+                return;
+            }else{
+                return $result;
+            }
+        }else{
+            $response["error"] = $user;
+            return $response;
+        }
+    }
+
+    function delete_event($username, $password, $calid, $eventid){
+        $access_token = $GLOBALS["tokens"]["edit"]["access_token"];
+        $refresh_token = $GLOBALS["tokens"]["edit"]["refresh_token"];
+        $token_type = $GLOBALS["tokens"]["edit"]["token_type"];
+        $client_id = $GLOBALS["tokens"]["edit"]["client_id"];
+        $client_secret = $GLOBALS["tokens"]["edit"]["client_secret"];
+        $user = verifyapi($username, $password);
+        if(is_array($user) && $user["perms"]["calendar.administration"]){
+            $i = 0;
+            begin:
+            $i++;
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://www.googleapis.com/calendar/v3/calendars/$calid/events/$eventid",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "DELETE",
+                // CURLOPT_POSTFIELDS => "{\"end\": {\"dateTime\":\"$end\"},\"start\":{\"dateTime\":\"$start\"},\"summary\":\"$summary\"}",
+                CURLOPT_HTTPHEADER => [
+                    "Authorization: ".$token_type." ".$access_token
+                ],
+            ]);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            $result = json_decode($response, true);
+            if($result["error"]["code"] == 401) {
+                $access_token = getnewtoken($refresh_token, $client_id, $client_secret);
+                if(isset($access_token)){
+                    $GLOBALS["tokens"]["edit"]["access_token"] = $access_token;;
+                    file_put_contents(realpath($_SERVER["DOCUMENT_ROOT"])."/secrets/GoogleApisecrets.json", json_encode($GLOBALS["tokens"]));
+                }
+                if($i < 2){
+                    goto begin;
+                }
+                return;
+            }else{
+                return $result;
+            }
+        }else{
+            $response["error"] = $user;
+            return $response;
+        }
+    }
 
     if($app == "updateperm"){
         $user = verifyapi($username, $password);
@@ -243,6 +465,12 @@
         $keys = array_column($event_array, 'start');
         array_multisort($keys, SORT_ASC, $event_array);
         $response["data"] = $event_array;
+    }elseif($app == "create_event"){
+        $response["data"] = create_event($username, $password, $_POST["calid"], $_POST["title"], $_POST["description"], $_POST["location"], $_POST["start"], $_POST["end"], filter_var($_POST["isdayevent"], FILTER_VALIDATE_BOOLEAN));
+    }elseif($app == "update_event"){
+        $response["data"] = create_event($username, $password, $_POST["calid"], $_POST["eventid"], $_POST["title"], $_POST["description"], $_POST["location"], $_POST["start"], $_POST["end"], filter_var($_POST["isdayevent"], FILTER_VALIDATE_BOOLEAN));
+    }elseif($app == "delete_event"){
+        $response["data"] = delete_event($username, $password, $_POST["calid"], $_POST["eventid"]);
     }else{
         http_response_code(404);
         $response["error"] = "Application unknown";
